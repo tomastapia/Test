3a202f20f73b3aece7f69a81c76cfb14f4a473f7
tomastapia <tomastapia@gmail.com>
2024-02-08 17:12:20 -0300

    Cambio de carpeta JS

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+
+numA = 20;
+numB = 30;
+
+resultado = numA + numB;
+
+console.log(resultado);
+
+// let autorizado = true;
+// // autorizado = false;
+
+// if(autorizado) {
+//     document.write('Bienvenido')
+// } else {
+//     alert('No autorizado')
+// }
+
+// console.log(autorizado);
+
+// function sumar(a, b) { return a + b; }
+
+// sumar(1, 2);
+
+
+
+// let lista = [1,2,3,4,5,6];
+
+// for (elemento of lista) { 
+//     let suma = sumar(elemento,5);
+//     console.log(suma);
+// }
+
+// // POO
+
+// // let animal = {
+// //     nombre: 'perro',
+// //     sonido: function sonido() { console.log('woof') }
+// // }
+
+// // console.log(animal.nombre);
+// // animal.sonido();
+
+// ///// =========================================
+
+// class Animal {
+//     constructor(nombre, sonido) {
+//         this.nombre = nombre;
+//         this.sonido = sonido;
+//     }
+//     hazSonido() {
+//         console.log('Soy un ' + this.nombre + ' y ' + this.sonido);
+//     }
+// }
+
+// let perro = new Animal('perro', 'woof');
+// let gato = new Animal('gato', 'miau');
+// let tigre = new Animal('tigre', 'grrr');
+
+// perro.hazSonido()
+// gato.hazSonido()
+// tigre.hazSonido()
+
+
+
+
+
+
